265acf9c81e416c2d2313f624961e745f5d2ed64
Tomislav Prstec <toprstec@vsite.hr>
2024-11-14 18:54:12 +0100

    Added generated controller and views.

--- MVCLibrary/Program.cs
+++ MVCLibrary/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCLibrary.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext") ?? throw new InvalidOperationException("Connection string 'DataContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
