9695f9a1b1eb3509e362f9e76a2d1a7157df664e
Tomislav Prstec <toprstec@vsite.hr>
2024-11-21 17:41:12 +0100

    Added Author field.

--- MVCLibrary/Controllers/BooksController.cs
+++ MVCLibrary/Controllers/BooksController.cs
@@ -54,7 +54,7 @@ namespace MVCLibrary.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ISBN")] Book book)
+        public async Task<IActionResult> Create([Bind("Id,Title,ISBN,Author")] Book book)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace MVCLibrary.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ISBN")] Book book)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ISBN,Author")] Book book)
         {
             if (id != book.Id)
             {
